7ce08f0027ddebb5eee1cfa409ebaa3fc0b25cb2
moiven <mruiz@mymail.mines.edu>
2015-10-10 12:26:54 -0600

    initial commit

---
+++ controller_code/controller_code.ino
@@ -0,0 +1,148 @@
+#include <NESpad.h>
+#include <SPI.h>
+#include "RF24.h"
+#include <LiquidCrystal.h>
+
+/**
+This is the code for controlling the robot wirelessly. The controller operates using an arduino Uno.
+The direction and button control comes from an NES game controller. An lcd display will display
+the status of the robot and status of the controller. The controller operates the robot via a wireless 2.4Gh
+signal coming from the RF24L01 transceiver.
+**/
+
+/*
+********The NES PAD*******
+* NES strobe pin -> pin 2
+* NES clock pin -> pin 3
+* NES data pin -> pin 4
+*
+***The RF24L01 transmitter***
+*
+*
+*****The LCD display*****
+* LCD pin 1 -> ground
+* LCD pin 2 -> 5V
+* LCD pin 3 -> 10k pot
+* LCD pin 4 -> pin A1
+* LCD pin 5 -> ground
+* LCD pin 6 -> pin A2
+* LCD pin 9 -> pin 5
+* LCD pin 11 -> pin A0
+* LCD pin 12 -> pin 6
+* LCD pin 13 -> pin 7
+* LCD pin 14 -> pin A1
+* LCD pin 15 -> 5V
+* LCD pin 16 -> gnd
+*/
+
+//controller send commands
+#define UP          1
+#define DOWN        2
+#define LEFT        3
+#define RIGHT       4
+#define A           5
+#define B           6
+#define START       7
+#define SELECT      8
+
+#define PRESSED     1
+
+static String history;
+
+//Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+RF24 radio(9,10);
+
+//initialize the NES library with the numbers of the interface pins
+NESpad nintendo = NESpad(2,3,4); //(strobe, clock, data);
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(A1, A2, 5, A0, 6, 7);
+
+byte addresses[][6] = {"8Node","9Node"};
+byte state = 0; // Buttons state (it ends up looking like a bitmap with 1's for buttons that are pressed and 0's for unpressed)
+
+void setup() 
+{  
+  lcd.begin(16, 2); 
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+  
+  // Start the radio listening for data
+  radio.stopListening();
+  //notify when the arduino is ready
+  lcd.print("Ready");
+}
+
+//send command over wireless
+//print to lcd screen
+void sendCommand(int command, String message)
+{
+  //send and print command
+  //but print "fails" if it fails
+  if(!radio.write(&command, sizeof(int)))
+    message = "failed";
+  if(history != message)
+  {
+    lcd.clear();
+    lcd.print(message);
+    history = message;
+  }
+}
+//read radio transmission
+/*void readRadio()
+{
+  int num;
+  float omega;
+  int gyroXTemp;
+  int gyroZTemp;
+  
+  //start listening for any data
+  radio.startListening();
+  if(radio.available())
+  {
+    radio.read(&num, sizeof(int));
+    //radio.read(&omega, sizeof(float));
+    //radio.read(&gyroXTemp, sizeof(int));
+    //radio.read(&gyroZTemp, sizeof(int));
+  }
+  //resume not listening
+  radio.stopListening();
+  //print the data to the lcd
+  //lcd.clear();
+  //move cursor to column 0, row 1
+  lcd.setCursor(0,1);
+  lcd.print(num);
+  //lcd.print(omega);
+  //lcd.print(gyroXTemp);
+  //lcd.print(gyroZTemp);
+}*/
+void loop() 
+{  
+  //readRadio();
+  state = nintendo.buttons();
+
+  // if the button is pressed right now and it's last state was UNPRESSED...
+  if(state & NES_A) 
+    sendCommand(A, "A");          //send command A
+  if(state & NES_B)
+    sendCommand(B, "B");          //send command B
+  if(state & NES_UP) 
+    sendCommand(UP, "UP");          //send command UP
+  if(state & NES_LEFT)
+    sendCommand(LEFT, "LEFT");          //send command LEFT
+  if(state & NES_RIGHT)
+    sendCommand(RIGHT, "RIGHT");          //send command RIGH
+  if((state & NES_DOWN))
+    sendCommand(DOWN, "DOWN");          //send command
+  if(state & NES_SELECT)
+    sendCommand(SELECT, "SELECT");          //send command
+  if(state & NES_START)
+    sendCommand(START, "START");          //send command
+}
---
+++ robot_code/robot_code.ino
@@ -0,0 +1,235 @@
+//Include the RF24L01 wireless library
+#include <SPI.h>
+#include "RF24.h"
+//Include an Encoder library, that let's us easily read the encoders
+#include <Encoder.h>
+//the APDS9960 library by sparkfun
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+//Define pin locations with names
+#define enable      4      //Motor Enable for Motor 1
+#define m1dir       7      //Motor Direction for Motor 1
+#define m2dir       8      // Motor direction for Motor 2       
+#define m1speed     9      //Motor Speed for Motor 1
+#define m2speed     10      //Motor Speed for Motor 2
+#define encoderA_M1 2     //Encoder 1 for Motor 1
+#define encoderB_M1 3     //Encoder 2 for Motor 1
+#define encoderA_M2 18     //Encoder 1 for Motor 2
+#define encoderB_M2 19     //Encoder 2 for Motor 2
+#define gyroX       0
+#define gyroZ       1
+#define trigPin 30    //Trig
+#define echoPin 31    //Echo
+
+//controller recieve commands
+#define UP          1
+#define DOWN        2
+#define LEFT        3
+#define RIGHT       4
+#define A           5
+#define B           6
+#define START       7
+#define SELECT      8
+#define D_UNPRESSED 9
+
+// Define a variable encoderNew, as an instantaneous encoder reading
+#define encoderNew myEnc.read()
+#define gyroXNew analogRead( gyroX )
+#define gyroZNew analogRead(gyroZ )
+
+//use the APDS9960 Library from sparkfun
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+
+//Use both encoders
+Encoder myEnc(encoderA_M1, encoderB_M1);
+
+//Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 40 & 53
+RF24 radio(40,53);  //for the mega
+
+//The address is the same for both transevers
+byte addresses[][6] = {"8Node","9Node"};
+
+//Define variables necessary for our speed calcs
+unsigned long t, milliOld;
+unsigned long encoderOld = 0;
+unsigned long encoderTemp = 0;
+float omega =0;
+float omega2 =0;
+float ts = 0.0;
+int gyroXTemp = 0;
+int gyroZTemp = 0;
+bool lineTrackerState = false;
+bool disabled = false;
+
+
+//globals for the line sensor
+uint16_t ambient_light = 0;
+uint8_t proximity_data = 0;
+uint16_t red_light = 0;
+uint16_t green_light = 0;
+uint16_t blue_light = 0;
+
+//vars for US Ranger
+long duration, cm;
+/**************************************Setup*************************************/
+void setup() {
+  Serial.begin(9600);
+  // Set the enable pin high (necessary for driving the motor)
+  pinMode(enable, OUTPUT);
+  digitalWrite(enable, HIGH);
+  pinMode(m1dir, OUTPUT);
+  pinMode(m2dir, OUTPUT);
+  pinMode(m1speed, OUTPUT);
+  pinMode(m2speed, OUTPUT);
+
+  //Define gyroscope pins
+  pinMode( gyroX, INPUT);
+  pinMode( gyroZ, INPUT);
+
+  //starting the radio
+  radio.begin();
+  
+  //initialize the APDS-9960
+  apds.init();
+  //start running the APDS-9960 light sensor (no interrupts)
+  apds.enableLightSensor(false);
+
+  //initialize US Ranger
+    pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  // Set the PA Level low to prevent power supply related issues since this is a
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1,addresses[0]);
+  
+  // Start the radio listening for data
+  radio.startListening();
+}
+/*******************************drive motors**********************************/
+//function to drive both motors from given parameters
+void driveMotor(int m1_speed, int m2_speed, bool m1_dir, bool m2_dir)
+{
+  //dir is the direction the wheels spin
+  digitalWrite(m1dir, m1_dir);
+  digitalWrite(m2dir, m2_dir);
+  //the speed is from 0 (stopped) to 255 (max)
+  analogWrite(m1speed, m1_speed);
+  analogWrite(m2speed, m2_speed);
+}
+/*********************************read radio data*******************************/
+//read radio transmission from the controller
+int readCommand()
+{
+  int command;
+  static unsigned long tread ;
+  //read command from the controller if available
+  if(radio.available())
+  {
+    radio.read(&command, sizeof(int));
+    tread = millis();
+  }
+  else if((millis() - tread) > 100)
+  { 
+    Serial.println("failed to recieve");
+    command = D_UNPRESSED;
+    
+  }
+  return command;
+}
+/*********************************Send radio data********************************/
+//data to send back to the controller
+//this will be printed on controller's lcd
+/*void sendRadio (float omega, int gyroXTemp, int gyroZTemp)
+{
+  int num = 5;
+  //stop listening so it can send
+  radio.stopListening();
+  //send data
+  //radio.write(&omega, sizeof(float));
+  //radio.write(&gyroXTemp, sizeof(int));
+  //radio.write(&gyroZTemp, sizeof(int));
+  radio.write(&num, sizeof(int));
+  //resume listening
+  radio.startListening();
+}*/
+/***********************************Line tracing***********************************/
+//read the light levels (ambient)
+void lineTracker()
+{
+  //check if there is an ambient light reading
+  //if not exit the function
+  if(!apds.readAmbientLight(ambient_light) || !apds.readRedLight(red_light))
+    return;
+  
+  //TODO: write code on implementing line tracking  
+} 
+/**************************************The loop************************************/
+//This loop is designed to send a command to the motor every iteration
+// If it is before 1second, it sends o (off), afterwards it send 255(on).
+//Every 1ms it will take readings and calculate speed, then print out values
+void loop()
+{
+  //US Ranger ping for distance
+  //digitalWrite(trigPin, LOW);
+  //delayMicroseconds(5);
+  //digitalWrite(trigPin, HIGH);
+  //delayMicroseconds(10);
+  //digitalWrite(trigPin, LOW);
+  //duration = pulseIn(echoPin, HIGH);
+  //cm = (duration/2) / 29.1;
+  
+  //print distance
+  //Serial.print("Distance = ");
+  //Serial.print(cm);
+  //Serial.println("cm");
+  
+  int command = readCommand();
+  //if the line tracker state is true
+  //run the linetracking function
+  if(disabled)
+    lineTracker();
+  //get the command from the controller
+  //give robot commands
+  if(!disabled)
+  {
+    switch(command)
+    {
+      case UP:  //Drive FORWARD
+        driveMotor(250, 250, HIGH, HIGH);
+        break;
+      case DOWN:  //Drive BACKWARDS
+        driveMotor(250, 250, LOW, LOW);
+        break;
+      case LEFT:  //Turn LEFT
+        driveMotor(100, 100, HIGH, LOW);
+        break;
+      case RIGHT:  //Turn RIGHT
+        driveMotor(100, 100, LOW, HIGH);
+        break;
+      case D_UNPRESSED:
+        driveMotor(0, 0, LOW, HIGH);
+        break;
+    }
+  }
+  switch(command)
+  {
+    case A:  //A BUTTON
+      //toggle the lineTracker function
+      !disabled;
+      break;
+    case B:  //B BUTTON
+      //do something
+      break;
+    case START:
+      //do something
+      break;
+    case SELECT:
+      //do something
+      break;  
+  }
+}
